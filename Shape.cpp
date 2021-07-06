76e5a7e9453b6a6050448c8369db8719ff3ed253
Erica <eluksts@gmail.com>
2021-07-06 19:35:05 -0400

    initial method definitions, prototypes, and class definition

--- Shape.cpp
+++ Shape.cpp
@@ -1,1 +1,54 @@
 //methods for shape class
+
+#include <iostream>
+#include <string>
+#include "Shape.h"
+
+using namespace std;
+
+Shape::Shape(string nameInput, string colourInput)
+{
+	name = nameInput;
+	colour = colourInput;
+}
+
+//default constructor - confirm this
+
+
+string Shape::GetName(void)
+{
+	return name;
+}
+
+string Shape::GetColour(void)
+{
+	return colour;
+}
+//watch return data type? don't want to change data member's value
+
+//also need to validate the input - if invalid leave data as it was
+void Shape::SetName(string nameInput)
+{
+	name = nameInput;
+}
+
+void Shape::SetColour(string colourInput)
+{
+	colour = colourInput;
+}
+
+//3 pure virtual functions
+double Shape::Perimeter(void)
+{
+	return 6.0;
+}
+
+double Shape::Area(void)
+{
+	return 6.0;
+}
+
+double Shape::OverallDimension(void)
+{
+	return 6.0;
+}
--- Shape.h
+++ Shape.h
@@ -1,2 +1,43 @@
-#pragma once
 //class definition for shape
+
+#include <iostream>
+#include <string>
+
+
+class Shape
+{
+private:
+	std::string name; 
+	//max 50 characters
+	//can be Circle Square, or unknown
+
+	std::string colour;
+	//max 10 characters
+	//allowed colours include red, green, blue, yellow, purple, pink, orange and undefined
+	
+public:
+
+	Shape(std::string nameInput, std::string colourInput);
+
+	//default constructor - confirm this
+
+	std::string GetName(void);
+
+	std::string GetColour(void);
+	//watch return data type? don't want to change data member's value
+
+	//also need to validate the input - if invalid leave data as it was
+	void SetName(std::string nameInput);
+
+	void SetColour(std::string colourInput);
+
+	//3 pure virtual functions
+	double Perimeter(void);
+
+	double Area(void);
+
+	double OverallDimension(void);
+
+	//no destructor needed
+
+};
