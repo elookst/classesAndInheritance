1c3fe94ecc071dfa0a0b780ba8dd232b7fe05b97
Erica <eluksts@gmail.com>
2021-07-06 20:27:15 -0400

    square methods and class definition

--- Square.cpp
+++ Square.cpp
@@ -1,1 +1,48 @@
 //methods for square class
+#include <iostream>
+#include <string>
+#include "Shape.h"
+#include "Square.h"
+
+
+using namespace std;
+
+
+Square::Square(string colour, float sideLengthInput)
+{
+
+}
+
+//default constructor
+Square::Square()
+{
+
+}
+
+Square::~Square()
+{
+	cout << "The square is squished ...";
+}
+
+
+float Square::GetSideLength(void)
+{
+	return sideLength;
+}
+
+void Square::SetSideLength(float sideLengthInput)
+{
+	sideLength = sideLengthInput;
+}
+//set the attribute if valid only
+
+int Show(void)
+{
+	return 1;
+}
+//display properties (inherit maybe?)
+
+//p = 4s
+// a = s^2
+
+//Overall Dimension should return side length of square (s)
--- Square.h
+++ Square.h
@@ -1,2 +1,36 @@
 #pragma once
 //definition for square
+
+#include <iostream>
+#include <string>
+#include "Shape.h"
+
+class Square : public Shape
+{
+private:
+	float sideLength;
+	//val must be greater than or equal to 0
+
+public:
+	Square(std::string colour, float sideLengthInput);
+
+	//default constructor
+	Square();
+
+	~Square();
+
+	float GetSideLength(void);
+
+	void SetSideLength(float sideLengthInput);
+	//set the attribute if valid only
+
+	int Show(void);
+	//display properties (inherit maybe?)
+
+	//p = 4s
+	// a = s^2
+	
+	//Overall Dimension should return side length of square (s)
+
+
+};
